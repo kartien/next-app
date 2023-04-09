36b30fa6445ad577277755cc308edd006d3b6ef1
kartien <kevinramon523@gmail.com>
2023-04-08 20:54:11 -0500

    Init new project

---
+++ app/about/page.jsx
@@ -0,0 +1,10 @@
+function AboutPage() {
+    return (
+        <div>
+            <h2>About Page</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore voluptate perferendis repellat corporis at ea a mollitia magnam, laborum repudiandae dolorum nam distinctio optio tenetur, eius dolores. Nesciunt, consequuntur atque.</p>
+        </div>
+    )
+}
+
+export default AboutPage
---
+++ app/services/page.jsx
@@ -0,0 +1,9 @@
+function ServicePage() {
+    return (
+        <div>
+            <h2>Services Page</h2>
+        </div>
+    )
+}
+
+export default ServicePage
