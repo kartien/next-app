29b1bfcb9814a0851ecb27bbcd578dce879fe5d5
kartien <kevinramon523@gmail.com>
2023-04-09 11:11:37 -0500

    ass server conponent

--- app/users/[id]/page.jsx
+++ app/users/[id]/page.jsx
@@ -1,7 +1,22 @@
-function UsersPage() {
+async function getUser(id) {
+  const res = await fetch(`https://reqres.in/api/users/${id}`)
+  const data = await res.json()
+  return data.data
+  
+}
+
+async function UsersPage({params}) {
+  const user = await getUser(params.id)
   return (
     <div>
       <h1>User detail</h1>
+      <div>
+        <img src={user.avatar} />
+        <div>
+          <h3>{user.id} {user.fist_name} {user.last_name}</h3>
+          <p>{ user.email }</p>
+        </div>
+      </div>
     </div>
   )
 }
